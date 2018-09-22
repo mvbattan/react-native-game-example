20a2d18dbe8eab7c81ca3826dac484068af038bb
mvbattan <mvbattan@gmail.com>
2018-09-22 15:10:09 -0300

    Adding responsiveness to pad

--- src/app/components/Joystick/components/Pad/index.js
+++ src/app/components/Joystick/components/Pad/index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 import styles from './styles';
 
 const Props = {};
 
 class Pad extends Component<Props> {
+  state = { fingerPosition: new Animated.ValueXY(0, 0) };
+
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (e, gE) => {
+      const { fingerPosition } = this.state;
+      const mockEvt = { dx: Math.min(Math.max(gE.dx, -50), 50), dy: Math.min(Math.max(gE.dy, -50), 50) };
+      Animated.event([{ dx: fingerPosition.x, dy: fingerPosition.y }], { useNativeEventDriver: true })(mockEvt);
+    },
+    onPanResponderTerminationRequest: () => true,
+    onPanResponderRelease: () => {
+      const { fingerPosition } = this.state;
+      Animated.timing(fingerPosition.x, { toValue: 0, duration: 300 }).start();
+      Animated.timing(fingerPosition.y, { toValue: 0, duration: 300 }).start();
+    },
+    onPanResponderTerminate: () => {
+      const { fingerPosition } = this.state;
+      Animated.timing(fingerPosition.x, { toValue: 0, duration: 300 }).start();
+      Animated.timing(fingerPosition.y, { toValue: 0, duration: 300 }).start();
+    }
+  });
+
   render() {
-    return <View style={styles.pad} />;
+    const { fingerPosition } = this.state;
+    return (
+      <View style={styles.pad}>
+        <View style={styles.actionRadius} />
+        <Animated.View
+          {...this.panResponder.panHandlers}
+          style={[styles.finger, { transform: fingerPosition.getTranslateTransform() }]}
+        />
+      </View>
+    );
   }
 }
 
--- src/app/components/Joystick/components/Pad/styles.js
+++ src/app/components/Joystick/components/Pad/styles.js
@@ -1,9 +1,26 @@
 import { StyleSheet } from 'react-native';
 
+const FINGER_SIZE = 25;
+
 const styles = StyleSheet.create({
   pad: {
     flex: 1,
-    backgroundColor: 'blue'
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  actionRadius: {
+    height: FINGER_SIZE * 6,
+    width: FINGER_SIZE * 6,
+    borderRadius: FINGER_SIZE * 3,
+    backgroundColor: 'red'
+  },
+  finger: {
+    height: FINGER_SIZE,
+    width: FINGER_SIZE,
+    backgroundColor: 'white',
+    borderRadius: FINGER_SIZE / 2,
+    position: 'absolute'
   }
 });
 
